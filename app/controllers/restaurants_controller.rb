dcd2c8fbf4fd4fe646bd81400073e8a0d847f0da
LeeAnne Ward <leeanneward@Dallass-iMac.local>
2016-05-10 15:47:23 -0500

    review links

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,6 +10,9 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1
   # GET /restaurants/1.json
   def show
+
+@reviews = Review.where(restaurant_id: @restaurant.id)
+    
   end
 
   # GET /restaurants/new
